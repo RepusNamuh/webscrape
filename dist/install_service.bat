064a12f66e6522a168ddc1f2fcfc9925b98ded0b
Repus <congle310610@gmail.com>
2025-06-30 19:47:10 +1000

    Get the service to pause after the script ran.

--- dist/install_service.bat
+++ dist/install_service.bat
@@ -3,6 +3,7 @@ echo Installing Web Scraper Service...
 schtasks /create /tn "WebScraperService" /tr "%CD%\BunningsScraper.exe service" /sc onstart /ru SYSTEM
 if %errorlevel% neq 0 (
     echo Failed to install service. Please run this script as an administrator.
+    pause
     exit /b 1
 )
 echo Installation complete.
